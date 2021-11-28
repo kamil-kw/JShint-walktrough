443b7d0ae4e565dc9d14bbeecdf9f74805bf4a43
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-11-28 18:05:32 +0000

    add json check

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,15 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
