e00f33c0d0da7a0fc9b3db86de8caf8818fbc8a5
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-11-29 20:09:07 +0000

    Add display exception

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,22 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+
+function processOptions(form) {
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
                         method: "POST",
@@ -21,9 +35,9 @@ async function postForm(e) {
                         if (response.ok) {
                             displayErrors(data);
                         } else {
+                            displayException(data);
                             throw new Error(data.error);
                         }
-
 }
 
 function displayErrors(data) {
@@ -38,9 +52,6 @@ function displayErrors(data) {
             results += `<div>Alt line <span class="line">${error.line}</span>,`;
             results += `column <span class="column">${error.col}</span></div>`;
             results += `<div class="error">${error.error}</div>`;
-
-
-
         }
     }
     document.getElementById("resultsModalTitle").innerText = heading;
@@ -59,6 +70,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data.expiry);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
@@ -73,4 +85,14 @@ function displayStatus(data) {
     document.getElementById("results-content").innerText = results;
 
     resultsModal.show();
+}
+
+function displayException(data) {
+
+    let heading = `An Exception Occured`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results +=
+
 }
